b6e2b77e6d4ccbaf234f073ab64e5ad79164ccfa
fengfangxiang <fengfangxiang@oneniceapp.com>
2017-08-25 12:20:28 +0800

    add example for training multilabel softmax

---
+++ slim/scripts/run_multilabel_softmax.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=0,1
+export TF_ENABLE_WINOGRAD_NONFUSED=1
+dataset=${1}
+model_name=resnet_v1_50
+
+train_dir=/raid/home/fengfangxiang/slim_log/${model_name}_${dataset}_softmax
+#pretrain_dir=/raid/data/public/slim_pretrained_model/resnet_v1_50.ckpt 
+
+mkdir -p ${train_dir}
+echo $train_dir
+echo $pretrain_dir
+$HOME/installed/python_tf_1.3.0-rc2/bin/python -u train_image_classifier.py \
+    --train_dir=${train_dir} \
+    --dataset_name=${dataset} \
+    --dataset_split_name=train \
+    --dataset_dir=/raid/home/fengfangxiang/gitcode/ncv_train_data/label2image/${dataset}/image \
+    --model_name=${model_name} \
+    --num_clones=2 \
+    --preprocessing_label_type=dense_normalize \
+    --learning_rate=0.00001 
+#    --checkpoint_exclude_scopes=resnet_v1_50/logits \
+#    --checkpoint_path=$pretrain_dir
